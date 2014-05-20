252d57cae7a72019fbc08ce644b60a54014761be
Bingsheng Zhang <bingsheng@Bingshengs-Mac-mini.local>
2014-05-20 14:07:44 +0300

    p521

--- KeyGen.cpp
+++ KeyGen.cpp
@@ -1,6 +1,5 @@
 /*
- *   Example program demonstrates 1024 bit Diffie-Hellman, El Gamal and RSA
- *   and 168 bit Elliptic Curve Diffie-Hellman 
+ *   p521
  *
  *   Requires: big.cpp ecn.cpp
  */
@@ -16,18 +15,23 @@
 #define  PKFILE "EC_PK.txt"
 #define  SKFILE "EC_SK.txt"
 
-/* NIST p192 bit elliptic curve prime 2#192-2#64-1 */
+/* NIST p521 bit elliptic curve prime 2#521-1 */
 
-char *ecp=(char *)"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFFFFFFFFFFFF";
+char *ecp=(char *)"000001FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
+
+/* elliptic curve parameter A */
+
+char *eca=(char *)"000001FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC";
+
 
 /* elliptic curve parameter B */
 
-char *ecb=(char *)"64210519E59C80E70FA7E9AB72243049FEB8DEECC146B9B1";
+char *ecb=(char *)"00000051953eb9618e1c9a1f929a21a0b68540eea2da725b99b315f3b8b489918ef109e156193951ec7e937b1652c0bd3bb1bf073573df883d2c34f1ef451fd46b503f00";
 
 /* elliptic curve - point of prime order (x,y) */
 
-char *ecx=(char *)"188DA80EB03090F67CBF20EB43A18800F4FF0AFD82FF1012";
-char *ecy=(char *)"07192B95FFC8DA78631011ED6B24CDD573F977A11E794811";
+char *ecx=(char *)"000000c6858e06b70404e9cd9e3ecb662395b4429c648139053fb521f828af606b4d3dbaa14b5e77efe75928fe1dc127a2ffa8de3348b3c1856a429bf97e7e31c2e5bd66";
+char *ecy=(char *)"0000011839296a789a3bc0045c8a5fb42c7d1bd998f54449579b446817afbd17273e662c97ee72995ef42640c550b9013fad0761353c7086a272c24088be94769fd16650";
 
 #ifndef MR_NOFULLWIDTH
 Miracl precision(50,0);
@@ -52,7 +56,7 @@ int main(int argc, char *argv[])
     irand((long)seed);   /* change parameter for different values */
 
     cout << "Generating EC-ElGamal Public and Private Key Pairs...." << endl;
-    a=-3;
+    a=-3; // a = p521 -3
     mip->IOBASE=16;
     b=ecb;
     p=ecp;
@@ -61,7 +65,7 @@ int main(int argc, char *argv[])
     y=ecy;
     g=ECn(x,y);
     //Generate and output pk and sk
-    sk=rand(160,2);
+    sk=rand(500,2);
     mip->IOBASE=64;
     fout.open(SKFILE);
     fout<<sk;
--- Verify.cpp
+++ Verify.cpp
@@ -1,6 +1,5 @@
 /*
- *   Example program demonstrates 1024 bit Diffie-Hellman, El Gamal and RSA
- *   and 168 bit Elliptic Curve Diffie-Hellman 
+ *   p521
  *
  *   Requires: big.cpp ecn.cpp
  */
@@ -19,18 +18,28 @@
 #define  PFILE "EC_plain.txt"
 
 
-/* NIST p192 bit elliptic curve prime 2#192-2#64-1 */
+/* NIST p521 bit elliptic curve prime 2#521-1 */
 
-char *ecp=(char *)"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFFFFFFFFFFFF";
+char *ecp=(char *)"000001FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF";
+
+/* group order q */
+
+char *ecq=(char *)"000001fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffa51868783bf2f966b7fcc0148f709a5d03bb5c9b8899c47aebb6fb71e91386409";
+
+/* elliptic curve parameter A */
+
+char *eca=(char *)"000001FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFC";
+
 
 /* elliptic curve parameter B */
 
-char *ecb=(char *)"64210519E59C80E70FA7E9AB72243049FEB8DEECC146B9B1";
+char *ecb=(char *)"00000051953eb9618e1c9a1f929a21a0b68540eea2da725b99b315f3b8b489918ef109e156193951ec7e937b1652c0bd3bb1bf073573df883d2c34f1ef451fd46b503f00";
 
 /* elliptic curve - point of prime order (x,y) */
 
-char *ecx=(char *)"188DA80EB03090F67CBF20EB43A18800F4FF0AFD82FF1012";
-char *ecy=(char *)"07192B95FFC8DA78631011ED6B24CDD573F977A11E794811";
+char *ecx=(char *)"000000c6858e06b70404e9cd9e3ecb662395b4429c648139053fb521f828af606b4d3dbaa14b5e77efe75928fe1dc127a2ffa8de3348b3c1856a429bf97e7e31c2e5bd66";
+char *ecy=(char *)"0000011839296a789a3bc0045c8a5fb42c7d1bd998f54449579b446817afbd17273e662c97ee72995ef42640c550b9013fad0761353c7086a272c24088be94769fd16650";
+
 
 #ifndef MR_NOFULLWIDTH
 Miracl precision(50,0);
